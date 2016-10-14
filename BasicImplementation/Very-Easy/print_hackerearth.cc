237f29e49f0b12a2f0837edb85648c0f68e88c31
karunakarhv <kvenkatesh@riverbed.com>
2016-10-14 16:56:05 +0530

    Print HackerEarth

--- BasicImplementation/Very-Easy/print_hackerearth.cc
+++ BasicImplementation/Very-Easy/print_hackerearth.cc
@@ -4,14 +4,55 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
 
-  char *hack_str = "hackerearth";
   char char_array[1000000];
-  int N, count = 0;
+  int N, count_hack = 0;
 
   cin >> N;
   for(int i=0; i< N; i++)
     cin >> char_array[i];
-  char_array[i] = '\n';
+  char_array[i] = '\0';
+  for(int i =0;i<N;i++)
+  {
+    switch(array[i])
+    {
+      case 'h':
+        count_h++;
+      break;
+      case 'a':
+        count_a++;
+      break;
+      case 'c':
+        count_c++;
+      break;
+      case 'k':
+        count_k++;
+      break;
+      case 'e':
+        count_e++;
+      break;
+      case 'r':
+        count_r++;
+      break;
+      case 't':
+        count_tt++;
+      break;
+    }
+    if((count_h==2) &&
+       (count_a==2) &&
+       (count_c==1) &&
+       (count_k==1) &&
+       (count_e==2) &&
+       (count_r==2) &&
+       (count_tt==1))
+    {
+      count_hack++;
+      count_h=0;
+      count_a=0;
+      count_c=0;
+      count_k=0;count_e=0;count_r=0;count_tt=0;
+    }
+  }
+  cout << count_hack << endl;
 
   /* code */
   return 0;
